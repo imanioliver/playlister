4d467cc6f34a316d35923ce05173de69143deda8
Imani Oliver <imani.oliver@gmail.com>
2018-09-07 21:25:36 -0400

    Update card titles

--- src/Components/PlaylistItem.js
+++ src/Components/PlaylistItem.js
@@ -6,8 +6,8 @@ export default class PlaylistItem extends Component {
             <div className="card card-inverse card-warning mb-3">
                 <div className="card-block">
                     <p className="card-text">User: {this.props.aSong.userName}</p>
-                    <p className="card-text">Artist/Band: {this.props.aSong.songArtist}</p>
-                    <p className="card-text">Title: {this.props.aSong.songTitle}</p>
+                    <p className="card-text">Artist: {this.props.aSong.songArtist}</p>
+                    <p className="card-text">Song: {this.props.aSong.songTitle}</p>
                     <p className="card-text">Notes: {this.props.aSong.songNotes}</p>
                 </div>
             </div>
